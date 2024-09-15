59f57e81812b551d6d826a18a7fa6cd173d29b00
Ali <alirazahaider2@gmail.com>
2024-09-15 22:01:29 +0500

    Added the functionality

--- Hidden-Search/index.js
+++ Hidden-Search/index.js
@@ -3,6 +3,23 @@ const container = document.createElement("div");
 container.className = `container`;
 
 
+
 const textInput = document.createElement("input");
+textInput.className = "active";
+textInput.placeholder = "Search...";
 document.body.appendChild(container);
 container.appendChild(textInput);
+
+const btn = document.createElement("button");
+
+const icon = document.createElement("i");
+icon.className = "fas fa-search"
+
+btn.appendChild(icon);
+btn.className = "icon-btn";
+container.appendChild(btn);
+
+
+btn.addEventListener("click", () => {
+    textInput.classList.toggle("active");
+})
