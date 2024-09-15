e93057898c7a9fdb34b93f475bf932832b6b5ad3
Ali <alirazahaider2@gmail.com>
2024-09-15 19:44:33 +0500

    Basic Layout added

---
+++ Hidden-Search/index.js
@@ -0,0 +1,8 @@
+
+const container = document.createElement("div");
+container.className = `container`;
+
+
+const textInput = document.createElement("input");
+document.body.appendChild(container);
+container.appendChild(textInput);
