68ca2ea170284915df06cb7674b50cfbc09ec73c
Ali <alirazahaider2@gmail.com>
2024-09-15 16:23:53 +0500

    Layouts improvement

--- Progress-Bar/index.js
+++ Progress-Bar/index.js
@@ -1,12 +1,22 @@
-const container = document.querySelector(".container")
-console.log(container)
+const container = document.querySelector(".container");
+console.log(container);
 for (let i = 0; i < 4; i++){
-    const circleEl = document.createElement("div")
-    circleEl.className = `circle${i + 1} circle`
-    circleEl.textContent = `${i+1}`
-    container.appendChild(circleEl)
+    const circleEl = document.createElement("div");
+    circleEl.className = `circle${i + 1} circle`;
+    circleEl.textContent = `${i + 1}`;
+    container.appendChild(circleEl);
 }
 
-const progressBar = document.createElement("div")
+const progressBar = document.createElement("div");
 progressBar.className = "progress"
-container.appendChild(progressBar)
+container.appendChild(progressBar);
+
+
+const prevBtn = document.createElement("button");
+const nextBtn = document.createElement("button");
+const btnContainer = document.querySelector(".nig");
+nextBtn.textContent = "Next"
+prevBtn.textContent = "Prev"
+
+btnContainer.appendChild(prevBtn);
+btnContainer.appendChild(nextBtn);
