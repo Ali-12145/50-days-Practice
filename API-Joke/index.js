b4a390ac3261176de2b8be789a4f4250cf2de5c7
Ali <alirazahaider2@gmail.com>
2024-09-16 22:28:21 +0500

    Layout Built

---
+++ API-Joke/index.js
@@ -0,0 +1,51 @@
+
+const container = document.createElement("div");
+container.className = "container";
+
+const heading = document.createElement("h2");
+heading.textContent = "Don't Laugh Challenge";
+
+
+const jokeText = document.createElement("div");
+jokeText.className = "joke"
+
+const btn = document.createElement("button");
+btn.textContent = "Another Joke";
+
+
+
+
+
+
+// btn.addEventListener("click", () => {
+//     const 
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.body.appendChild(container);
+container.appendChild(heading);
+container.appendChild(jokeText);
+container.appendChild(btn);
+
+
