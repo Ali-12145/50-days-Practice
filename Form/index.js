e468b760b82dc3256ba54871ced061146276b5b9
Ali <alirazahaider2@gmail.com>
2024-09-16 17:10:29 +0500

    Animation set

--- Form/index.js
+++ Form/index.js
@@ -10,23 +10,25 @@ bodyEl.appendChild(container);
 const formEl = document.createElement("form");
 
 const form_control1 = document.createElement("div");
+
 form_control1.className = "form-control";
-const email = document.createElement("input")
-email.type = email;
+
+const email = document.createElement("input");
+
+email.type = "text";
+
 email.required = true;
+
 const emailLabel = document.createElement("label");
+
 emailLabel.innerHTML = `<span style="transition-delay: 0ms">E</span>
                         <span style="transition-delay: 50ms">m</span>
                         <span style="transition-delay: 100ms">a</span>
                         <span style="transition-delay: 150ms">i</span>
                         <span style="transition-delay: 200ms">l</span>`;
 
-container.appendChild(formEl);
-formEl.appendChild(form_control1);
-form_control1.appendChild(emailLabel);
-form_control1.appendChild(email);
-
-
+                        form_control1.appendChild(email);
+                        form_control1.appendChild(emailLabel);
 
 const form_control2 = document.createElement("div");
 form_control2.className = "form-control";
@@ -35,18 +37,17 @@ password.type = "password"; // Changed to password
 password.required = true;
 const passLabel = document.createElement("label");
 passLabel.innerHTML = `<span style="transition-delay: 0ms">P</span>
-                       <span style="transition-delay: 50ms">A</span>
-                       <span style="transition-delay: 100ms">S</span>
-                       <span style="transition-delay: 150ms">S</span>
-                       <span style="transition-delay: 200ms">W</span>
-                       <span style="transition-delay: 250ms">O</span>
-                       <span style="transition-delay: 300ms">R</span>
-                       <span style="transition-delay: 350ms">D</span>`;
-
-container.appendChild(formEl);
-formEl.appendChild(form_control2);
+<span style="transition-delay: 50ms">A</span>
+<span style="transition-delay: 100ms">S</span>
+<span style="transition-delay: 150ms">S</span>
+<span style="transition-delay: 200ms">W</span>
+<span style="transition-delay: 250ms">O</span>
+<span style="transition-delay: 300ms">R</span>
+<span style="transition-delay: 350ms">D</span>`;
+form_control2.appendChild(password); // Changed to password
 form_control2.appendChild(passLabel); // Changed to passLabel
-form_control2.appendChild(password);  // Changed to password
 
 
-
+formEl.appendChild(form_control1);
+formEl.appendChild(form_control2);
+container.appendChild(formEl);
