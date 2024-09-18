d1ea55e9754e5b6943223dd475a5578b12f414ed
Ali <alirazahaider2@gmail.com>
2024-09-18 19:03:28 +0500

    Added css and JS

--- Split-Landing/index.js
+++ Split-Landing/index.js
@@ -1,2 +1,35 @@
 const container = document.createElement("div");
 container.className = "container";
+
+const left_side = document.createElement("div");
+left_side.className = " split left";
+const right_side = document.createElement("div");
+right_side.className = "split right";
+
+container.appendChild(left_side);
+container.appendChild(right_side);
+document.body.appendChild(container);
+
+
+
+left_side.addEventListener("mouseenter", () => {
+    left_side.style.width = "75%";
+    right_side.style.width = "25%";
+})
+left_side.addEventListener("mouseleave", () => {
+    left_side.style.width = "50%";
+    right_side.style.width = "50%";
+})
+
+right_side.addEventListener("mouseenter", () => {
+    right_side.style.width = "75%";
+    left_side.style.width = "25%";
+
+
+})
+right_side.addEventListener("mouseleave", () => {
+    right_side.style.width = "50%";
+    left_side.style.width = "50%";
+
+
+})
